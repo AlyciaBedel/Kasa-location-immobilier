fa2dd44ca72761f65822192e40b398d1e072d7d4
Alycia Bedel <bedel-alycia@outlook.fr>
2023-03-29 11:25:29 +0200

    erreur dans dossier

--- src/routes/RoutesPath.jsx
+++ src/routes/RoutesPath.jsx
@@ -4,11 +4,6 @@ import Layouts from "../layouts/Layouts"
 import Home from "../pages/home/Home";
 import Error404 from "../pages/error/Error404";
 
-// import ApartmentSheet from "../pages/ApartmentSheet/ApartmentSheet";
-// import About from "../pages/About/About";
-
-
-
 
 function RoutesPath() {
     return(
@@ -17,8 +12,8 @@ function RoutesPath() {
                 <Routes>
                     <Route element={<Navigate replace to="/accueil" />} path="/" />
                     <Route path="/accueil" element={<Home />}/>
-                    {/* <Route path="/logement/:id" element={<ApartmentSheet />}/>
-                    <Route path="/a-propos" element={<About />}/>*/}
+                    {/* <Route path="/apartment/:id" element={<ApartmentSheet />}/>
+                    <Route path="/about" element={<About />}/>*/}
                     <Route path="*" element={<Error404 />}/>
                 </Routes>
             </Layouts>
