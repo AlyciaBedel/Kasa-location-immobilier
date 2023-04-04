6b85b6d8df4ac9625827ac7fc51e6f3b6c6ea712
Alycia Bedel <bedel-alycia@outlook.fr>
2023-04-04 15:36:48 +0200

    bannière about

--- src/components/imageBanner/ImageBanner.jsx
+++ src/components/imageBanner/ImageBanner.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import './imageBanner.scss'
-
+import backgroundImg from '../../assets/img/aboutBanner.jpg'
 
 const ImageBanner = (props) => {
+    const imageUrl = props.imageUrl ? props.imageUrl : backgroundImg;
     return (
         <div className='banner__img'>
-			<img src={props.imageUrl} alt="" />
+			<img src={imageUrl} alt="" />
 		</div>
     );
 };
