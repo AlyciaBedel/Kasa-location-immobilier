77f4c1e3503cfeec8d0429fb3e15ce00824a764e
Alycia Bedel <bedel-alycia@outlook.fr>
2023-04-05 18:21:49 +0200

    Content of the dropdowns put in a table and installation of a .map to display them

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -4,14 +4,20 @@ import ImageBanner from '../../components/imageBanner/ImageBanner';
 import DescriptionPanel from '../../components/descriptionPanel/DescriptionPanel';
 
 const About = () => {
+    const collapseContent = [
+        "Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées  par nos équipes.",
+        "La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.",
+        "Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. N'hésitez pas à nous contacter si vous avez la moindre question.",
+        "La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes."
+    ];
+
     return (
         <>
             <ImageBanner/>
             <div className='about__container'>
-                <DescriptionPanel title="Fiabilité" content="La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme."/>
-                <DescriptionPanel title="Respect" content="La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme."/>
-                <DescriptionPanel title="Service" content="La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme."/>
-                <DescriptionPanel title="Responsabilité" content="La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme."/>
+                {collapseContent.map((content, index) => (
+                    <DescriptionPanel key={index} title={index % 2 === 0 ? "Fiabilité" : "Respect"} content={content}/>
+                ))}
             </div>
         </>
     );
