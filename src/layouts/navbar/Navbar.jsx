bf4d0395a4b7028381873fc4db7e9dafb34d1e66
Alycia Bedel <bedel-alycia@outlook.fr>
2023-04-27 19:03:20 +0200

    add focus menu nav and little responsive

--- src/layouts/navbar/Navbar.jsx
+++ src/layouts/navbar/Navbar.jsx
@@ -1,16 +1,22 @@
-import React from 'react'
-import './navbar.scss'
-import logo from '../../assets/img/logo.png'
-import { NavLink } from 'react-router-dom'
+import React, { useEffect, useRef } from 'react';
+import './navbar.scss';
+import logo from '../../assets/img/logo.png';
+import { NavLink } from 'react-router-dom';
 
 const Navbar = () => {
+  const linkRef = useRef(null); 
+  
+  useEffect(() => {
+    linkRef.current.focus(); 
+  }, []);
+
   return (
     <header>
       <NavLink to="/">
         <img src={logo} alt="logo de l'entreprise kasa" />
       </NavLink>
       <nav>
-        <NavLink to="/" className="linkNavbar">
+        <NavLink to="/" className="linkNavbar" ref={linkRef}>
           Accueil
         </NavLink>
         <NavLink to="/apropos" className="linkNavbar">
@@ -18,7 +24,7 @@ const Navbar = () => {
         </NavLink>
       </nav>
     </header>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
